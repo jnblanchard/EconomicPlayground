6b6672b382b9e9fb5c59ec3ac18451caa0245d77
jblanch2 <jnblanchard@mac.com>
2014-11-05 18:32:58 -0800

    first commit; Pulling from quandl // economics API. JSON serialization.

---
+++ EconomicPlayground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EconomicPlayground
+//
+//  Created by John Blanchard on 11/5/14.
+//  Copyright (c) 2014 John Blanchard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
