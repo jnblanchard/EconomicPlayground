f6da8261d1e8f1f81f89716f4852e9a74f712396
jblanch2 <jnblanchard@mac.com>
2014-11-17 11:30:45 -0800

    Exchanger Skeleton, testing needed.

--- EconomicPlayground/ExchangeViewController.m
+++ EconomicPlayground/ExchangeViewController.m
@@ -9,18 +9,21 @@
 #import "ExchangeViewController.h"
 #import "Data.h"
 //#import <ShinobiCharts/ShinobiCharts.h>
-
 @interface ExchangeViewController ()    //<SChartDatasource, SChartDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *usdTextField;
+@property (weak, nonatomic) IBOutlet UITextField *currencyTextField;
 @property NSMutableDictionary* dicts;
 @property NSMutableArray* dates;
 @property NSMutableArray* toUSDs;
 @property NSMutableArray* dataSet;
+@property double exchangeRate;
 @end
 
 @implementation ExchangeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.exchangeRate = 2;
     [self loadJSON];
 }
 
@@ -63,19 +66,16 @@
 
 - (NSString*) breakApartTuple:(NSString*) tuple
 {
-    if ([tuple isKindOfClass:[NSString class]]) {
-        NSLog(@"true");
-    }
-    Data* data = [Data new];
+//    Data* data = [Data new];
     return @"";
 }
 
-- (NSString*) loadDates:(NSArray*) temp
+- (void) loadDates:(NSArray*) temp
 {
     for (id tuple in temp) {
         [self breakApartTuple:tuple];
+        NSLog(@"At datapoint %lu, %@,", (unsigned long)[temp indexOfObject:tuple], tuple);
     }
-    return [NSString stringWithFormat:@"load dates finished"];
 }
 
 - (void) displayData:(NSArray*) tempTwo
@@ -83,6 +83,51 @@
 //    NSLog(@"Value of Dolar in (Date , Peso) format - \n\n%@", tempTwo);
 //    NSLog(@"%@", [tempTwo objectAtIndex:1]);
 //    NSLog(@"%@", self.dates);
+}
+
+- (IBAction)checkTextField:(UITextField*)sender
+{
+    NSNumberFormatter* formatter = [NSNumberFormatter new];
+    NSNumber* number = [formatter numberFromString:sender.text];
+    if (![sender.text isEqualToString:@""] && number != nil && [self.currencyTextField.text isEqualToString:@""])
+    {
+
+    } else {
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Cannot execute this conversion." message:@"Must have only one number input, and currency selected" delegate:self cancelButtonTitle:@"Dismiss" otherButtonTitles: nil];
+        [alert show];
+        self.usdTextField.text = @"";
+    }
+}
+
+- (IBAction)checkTextFieldTwo:(UITextField*)sender
+{
+    NSNumberFormatter* formatter = [NSNumberFormatter new];
+    NSNumber* number = [formatter numberFromString:sender.text];
+    if (![sender.text isEqualToString:@""] && number != nil && [self.usdTextField.text isEqualToString:@""])
+    {
+
+    } else {
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Cannot execute this conversion." message:@"Must have only one number input, and currency selected" delegate:self cancelButtonTitle:@"Dismiss" otherButtonTitles: nil];
+        [alert show];
+        self.currencyTextField.text = @"";
+    }
+}
+
+- (IBAction)currencyButtonHit:(id)sender
+{
+
+}
+
+- (IBAction)convertButtonPressed:(id)sender
+{
+    if ([self.usdTextField.text isEqualToString:@""])
+    {
+        NSLog(@"%@ pesos, is equivalent to $%00f USD", self.currencyTextField.text, self.currencyTextField.text.doubleValue/self.exchangeRate);
+    }
+    if ([self.currencyTextField.text isEqualToString:@""])
+    {
+        NSLog(@"$%@ USD, is equivalent to %00f pesos", self.usdTextField.text, self.usdTextField.text.doubleValue*self.exchangeRate);
+    }
 }
 
 @end
